ce857a345d473cc4e90c54595fef66884c9b71b8
Slupov <stoyan.lupov@gmail.com>
2018-12-26 13:58:48 +0200

    [Added forgotten file]

---
+++ OnlineFishShop.Web/Infrastructure/Extensions/Helpers/Secrets/AppKeyConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineFishShop.Web.Infrastructure.Extensions.Helpers.Secrets
+{
+    public class AppKeyConfig
+    {
+        public string FacebookAppId { get; set; }
+        public string FacebookAppSecret { get; set; }
+    }
+}
